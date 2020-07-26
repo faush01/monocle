b34e130b56de4292076e18633df765d091233448
shaun <shaun@bluebit.com.au>
2020-07-27 09:53:22 +1000

    add watchdog

--- scripts/logger.py
+++ scripts/logger.py
@@ -11,18 +11,28 @@ from threading import Thread
 # (3600 / (3200 / 1000)) / seconds = watts
 # time = (3600 / (3200 / 1000)) / watts
 # (3600 / (3200 / 1000)) / 5500 = 
+# (pulses / 3200) * (3600 / seconds) = Kw/h
+# ((pulses / 3200) * (3600 / seconds) * 1000) / 3600 = w/s
+# ((pulses / 3200) * (3600 / seconds)) / 3.6 = w/s
+# 9 * Pulse / 8 * Sec / 3.6 = w/s
+# ((9 * Pulse) / (8 * Sec)) / 3.6 = w/s
+# convert to watts per second
 
 
 def call_notify_thread(event_date, ws):
-    request_data = {
-        "table": "power_usage",
-        "event_date": event_date,
-        "event_type": "ws",
-        "event_data": ws
-    }
-    print ("Sending Data: " + str(request_data))
-    r = requests.post("http://192.168.0.16:3456", json=request_data)
-    # print ("New Data Notification Request : " + r.text)
+    try:
+        request_data = {
+            "table": "power_usage",
+            "event_date": event_date,
+            "event_type": "ws",
+            "event_data": ws
+        }
+        # print ("Sending Data: " + str(request_data))
+        r = requests.post("http://192.168.0.16:3456", json=request_data)
+        # print ("New Data Notification Request : " + r.text)
+
+    except Exception as err:
+        print("call_notify_thread - " + event_date + " - " + str(err))
 
 
 def log_data(time_stamp, pulse_count, time_span):
@@ -59,18 +69,19 @@ def log_data(time_stamp, pulse_count, time_span):
         t = Thread(target=call_notify_thread, args=(event_date, ws))
         t.start()
     except Exception as err:
-        print(str(err))
+        print("log_data - " + event_date + " - " + str(err))
 
 
 log_interval = 1000000000 * 60 # 60 seconds
 count = 1
 last_log_stamp = -1
 gpio_input_pin = 7
-
+last_pulse_detected = time.time()
 
 def button_callback(channel):
     global count
     global last_log_stamp
+    global last_pulse_detected
 
     pinval = GPIO.input(channel)
     if pinval == 1:
@@ -84,14 +95,15 @@ def button_callback(channel):
     time_diff = now_stamp - last_log_stamp
 
     if time_diff > log_interval:
-        print ("Logging Triggered : " + str(now_stamp) + " - " + str(count) + " - " + str(time_diff))
+        # print ("Logging Triggered : " + str(now_stamp) + " - " + str(count) + " - " + str(time_diff))
         log_data(now_stamp, count, time_diff)
         last_log_stamp = now_stamp
+        last_pulse_detected = time.time()
         count = 0
     else:
         count += 1
 
-    #print("Button was pushed : " + str(count) + " - " + str(time_diff) + " - " + str(datetime.datetime.now()))
+    # print("Button was pushed : " + str(count) + " - " + str(time_diff) + " - " + str(datetime.datetime.now()))
 
 
 # if not os.path.exists('log_files'):
@@ -116,7 +128,16 @@ GPIO.setmode(GPIO.BOARD)
 GPIO.setup(gpio_input_pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 GPIO.add_event_detect(gpio_input_pin, GPIO.FALLING, callback=button_callback, bouncetime=20)
 
-message = input("Press enter to quit\n\n")
+while True:
+
+    seconds_since_last_update = time.time() - last_pulse_detected
+    print ("Time since last update: %s" % seconds_since_last_update)
+    if seconds_since_last_update > 90:
+        print ("No new data for %s seconds" % seconds_since_last_update)
+    time.sleep(60)
+
+# message = input("Press enter to quit\n\n")
+
 GPIO.cleanup()
 
 
